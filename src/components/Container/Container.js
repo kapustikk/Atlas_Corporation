a7410baecb32fb1d08a2b3eff537b57f80122da6
Yulia Kovalenko <kus_queen@icloud.com>
2021-06-07 12:09:38 +0300

    fixed contacts

--- src/components/Container/Container.js
+++ src/components/Container/Container.js
@@ -1,5 +1,9 @@
 import s from '../Container/Container.module.css';
 
 export default function Container({ children }) {
-  return <div className={s.container}>{children}</div>;
+  return (
+    <div className={s.wrapper}>
+      <div className={s.container}>{children}</div>
+    </div>
+  );
 }
